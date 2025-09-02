3536ed78d1e03d97e48a65961c64e09125027680
Marsel Eustake <marsel90@yahoo.com>
2025-09-03 01:00:49 +0300

    Add files via upload

---
+++ Deemphasis MPX.js
@@ -0,0 +1,10 @@
+var pluginConfig = {
+    name: 'Deemphasis MPX',
+    version: '0.1',
+    author: 'Marsel90',
+    frontEndPath: 'Deemphasis MPX/deemphasismpx.js'
+}
+
+module.exports = {
+    pluginConfig
+}
